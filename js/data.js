3f3706d494ba3d48bdff2bb474b40e2cd5ff52b2
zatroxcs-ai <zatroxcs@gmail.com>
2025-11-29 01:29:59 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -7,7 +7,7 @@ export const APP_CONFIG = {
 
 export const initialGameData = {
     config: {
-        mapUrl: 'Game-master2/map.png',
+        mapUrl: 'github.com/zatroxcs-ai/Game-master2/tree/main/assets/map.png',
         mapName: 'Arène Royale'
     },
     players: [], // { id, name, avatar, gold, elixir, darkElixir, inventory, deck: [], x: 50, y: 50 }
